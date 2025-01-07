a3ed71169c3bd3ecf7e47257699593992d01ed05
Ionut Frisan <ionutfrisan@gmail.com>
2025-01-07 02:09:29 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,13 +1,21 @@
 using FrisanIonutLab7.Models;
-
+using System.Diagnostics;
 namespace FrisanIonutLab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -17,10 +25,36 @@ public partial class ListPage : ContentPage
         await Navigation.PopAsync();
     }
 
+    async void OnDeleteItemButonClicked(object sender, EventArgs e)
+    { 
+        if (listView.SelectedItem != null)
+        {
+            var slist = (ShopList)BindingContext;
+            var p = listView.SelectedItem as Product;
+            var products = await App.Database.GetListProductsAsync(slist.ID);
+            var product = products.Where(lp => lp.ID == p.ID).FirstOrDefault();
+            
+            if (product != null)
+            {
+                await App.Database.DeleteListProductAsync(slist.ID, product.ID);
+                listView.ItemsSource = await App.Database.GetListProductsAsync(slist.ID);
+            }
+        }
+        Debug.WriteLine("*****");
+    }
+
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
     }
 }
